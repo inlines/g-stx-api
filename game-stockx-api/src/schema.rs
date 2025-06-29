6db75d07357bd1ecffba256d83eb93c7bb6673bc
Закиров Кирилл <k.zakirov@webbankir.ru>
2025-06-29 23:31:05 +0300

    migration fix

--- game-stockx-api/src/schema.rs
+++ game-stockx-api/src/schema.rs
@@ -13,6 +13,15 @@ diesel::table! {
         abbreviation -> Nullable<Text>,
         name -> Text,
         generation -> Nullable<Int4>,
+        active -> Nullable<Bool>,
+        total_games -> Nullable<Int4>,
+    }
+}
+
+diesel::table! {
+    product_platforms (product_id, platform_id) {
+        product_id -> Int4,
+        platform_id -> Int4,
     }
 }
 
@@ -53,14 +62,63 @@ diesel::table! {
     }
 }
 
+diesel::table! {
+    screenshots (id) {
+        id -> Int4,
+        image_url -> Text,
+        game -> Int4,
+    }
+}
+
+diesel::table! {
+    users (id) {
+        id -> Int4,
+        user_login -> Text,
+        password_hash -> Text,
+        created_at -> Nullable<Timestamp>,
+    }
+}
+
+diesel::table! {
+    users_have_bids (release_id, user_login) {
+        release_id -> Int4,
+        user_login -> Text,
+    }
+}
+
+diesel::table! {
+    users_have_releases (release_id, user_login) {
+        release_id -> Int4,
+        user_login -> Text,
+    }
+}
+
+diesel::table! {
+    users_have_wishes (release_id, user_login) {
+        release_id -> Int4,
+        user_login -> Text,
+    }
+}
+
+diesel::joinable!(product_platforms -> platforms (platform_id));
+diesel::joinable!(product_platforms -> products (product_id));
 diesel::joinable!(releases -> products (product_id));
-diesel::joinable!(sales -> products (product_id));
+diesel::joinable!(screenshots -> products (game));
+diesel::joinable!(users_have_bids -> releases (release_id));
+diesel::joinable!(users_have_releases -> releases (release_id));
+diesel::joinable!(users_have_wishes -> releases (release_id));
 
 diesel::allow_tables_to_appear_in_same_query!(
     covers,
     platforms,
+    product_platforms,
     products,
     regions,
     releases,
     sales,
+    screenshots,
+    users,
+    users_have_bids,
+    users_have_releases,
+    users_have_wishes,
 );
